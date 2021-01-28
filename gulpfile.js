3b6c7213e00e7614fe4c6ba604fc8940234ad2c0
masato1983 <50264750+masato1983@users.noreply.github.com>
2021-01-28 16:22:21 +0900

    Ediit file gulpfile.js: Implement sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 const { src, dest } = require('gulp');
 const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 
 function styles() {
   return src(['./src/sass/**/*.scss', '!./src/sass/widget.scss'])
+    .pipe(sourcemaps.init())
     .pipe(sass())
+    .pipe(sourcemaps.write('.'))
     .pipe(dest('./dist/css'))
 }
 
