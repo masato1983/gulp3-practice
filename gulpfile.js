ae20fe4a7b64a7fe46825c825e133d9276845e12
masato1983 <50264750+masato1983@users.noreply.github.com>
2021-01-29 18:18:38 +0900

    Edit file gulpfile.js: Implement gulp-uglify

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,22 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync');
-const cleanCss = require('gulp-clean-css');
+const GulpCleanCss = require('gulp-clean-css');
+const GulpUglify = require('gulp-uglify');
 
 function styles() {
   return src(['./src/sass/**/*.scss', '!./src/sass/widget.scss'])
     .pipe(sourcemaps.init())
     .pipe(sass())
-    .pipe(cleanCss())
+    .pipe(GulpCleanCss())
     .pipe(sourcemaps.write('.'))
     .pipe(dest('./dist/css'))
+}
+
+function javascript() {
+  return src('./src/js/**/*.js')
+    .pipe(GulpUglify())
+    .pipe(dest('./dist/js'))
 }
 
 function serve() {
@@ -18,7 +25,7 @@ function serve() {
     server: './',
     browser: 'google chrome'
   })
-  watch(['./src/sass/**/*.scss', '**/*.html'], series(styles)).on('change', browserSync.reload)
+  watch(['./src/sass/**/*.scss', '**/*.html', './src/js/**/*.js'], series(styles, javascript)).on('change', browserSync.reload)
 }
 
 exports.styles = styles;
