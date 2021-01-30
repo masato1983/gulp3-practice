82f280dae7663132a33ca54ec62c6612658642a4
masato1983 <50264750+masato1983@users.noreply.github.com>
2021-01-30 20:12:59 +0900

    Edit file alert.js: Add ES6 syntax

--- src/js/alert.js
+++ src/js/alert.js
@@ -1,3 +1,9 @@
 // alert('The aleat.js was merged!!');
 
 document.querySelector('.title').textContent = 'The text from alert.js';
+
+const message = () => {
+  console.log('The Javascript was transpiled!');
+}
+
+message();
